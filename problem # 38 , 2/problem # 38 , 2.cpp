adb922d4e8954ccb2b4e5afdca906237f6c7abeb
KHOLUD AHMED <kholudahmed666@gmail.com>
2025-01-27 10:57:06 +0200

    second commit

---
+++ problem # 38 , 2/problem # 38 , 2.cpp
@@ -0,0 +1,65 @@
+
+
+#include <iostream>
+using namespace std;
+int Random_number(int from, int to)
+{
+    int Rand_num = rand() % (to - from + 1) + from;
+    return Rand_num;
+}
+void fill_array_with_random_numbers(int arr1[100], int& arrlength)
+{
+    cout << "\n Enter number of elements :" << endl;
+    cin >> arrlength;
+
+    for (int i = 0; i < arrlength; i++)
+    {
+        arr1[i] = Random_number(1, 100);
+    }
+
+}
+void add_array_element(int Number, int arr[100], int& arrlength)
+{
+    arrlength++;
+    arr[arrlength - 1] = Number;
+}
+void copy_odd_numbers(int arrsource[100], int arrdestination[100], int arrlength, int& arr2length)
+{
+    for (int i = 0; i < arrlength; i++)
+    {
+        if (arrsource[i] % 2 != 0)
+        {
+                add_array_element(arrsource[i], arrdestination, arr2length);
+        }
+
+    }
+}
+void print_array_with_random_numbers(int arr[100], int arrlength)
+{
+    for (int i = 0; i < arrlength; i++)
+    {
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr[100], arr2[100], arrlength = 0, arr2length = 0;
+
+    fill_array_with_random_numbers(arr, arrlength);
+
+    copy_odd_numbers(arr, arr2, arrlength, arr2length);
+
+    cout << "Array  1 elements : " << endl;
+    print_array_with_random_numbers(arr, arrlength);
+    cout << endl;
+
+    cout << "Array 2 odd numbers : " << endl;
+    print_array_with_random_numbers(arr2, arr2length);
+    cout << endl;
+
+
+}
